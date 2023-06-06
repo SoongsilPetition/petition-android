624222b6579125e6b293c77e71a816153a931f96
mochaive <cafemocha.dev@gmail.com>
2023-06-06 16:53:41 +0900

    add: BottomBar 적용

--- app/src/main/java/com/marassu/petition/MainActivity.kt
+++ app/src/main/java/com/marassu/petition/MainActivity.kt
@@ -1,21 +1,46 @@
 package com.marassu.petition
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
 import com.marassu.petition.base.BaseActivity
+import com.marassu.petition.view.atom.CustomBottomBar
+import com.marassu.petition.view.navigation.Screen
+import com.marassu.petition.view.navigation.SetUpBottomNavGraph
 import com.marassu.petition.view.theme.PetitionTheme
+
 
 class MainActivity : BaseActivity() {
 
     @Composable
     override fun Content() {
-        PetitionTheme() {
-            Greeting(name = "Android")
-        }
-    }
+        var currentRoute by remember { mutableStateOf(Screen.Home.route) }
+        val navController = rememberNavController()
 
-    @Composable
-    fun Greeting(name: String) {
-        Text(text = "Hello $name!")
+        PetitionTheme() {
+            Scaffold(bottomBar = {
+                CustomBottomBar(selectedRoute = currentRoute, onItemSelected = {
+                    currentRoute = it.route
+                    navController.popBackStack()
+                    navController.navigate(currentRoute)
+                })
+            }) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(bottom = it.calculateBottomPadding())
+                ) {}
+                SetUpBottomNavGraph(navController = navController)
+            }
+        }
     }
 }
