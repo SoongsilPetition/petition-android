9e872ced99914b8564b220d450e687722a0318cc
coding-polarbear <baehynebin@hotmail.co.kr>
2023-07-17 17:10:05 +0900

    chore : Home Screen Bottom bar 터치 개선

--- app/src/main/java/com/marassu/petition/feature/home/HomeScreen.kt
+++ app/src/main/java/com/marassu/petition/feature/home/HomeScreen.kt
@@ -3,11 +3,8 @@ package com.marassu.petition.feature.home
 import android.content.Intent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Divider
 import androidx.compose.material.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -16,16 +13,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.paging.LoadState
-import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
-import com.marassu.entity.petition.Petition
 import com.marassu.petition.feature.petition_detail.PetitionDetailActivity
 import com.marassu.petition.feature.write.WriteActivity
 import com.marassu.petition.view.atom.TitleTopBar
 import com.marassu.petition.view.petition_list.PetitionList
-import com.marassu.petition.view.petition_list.PetitionListItem
-import timber.log.Timber
 
 @Composable
 fun HomeScreen() {
@@ -52,16 +44,24 @@ fun HomeScreen() {
         }) { paddingValues ->
             Surface(
                 modifier = Modifier
-                    .padding(paddingValues)
+                    .padding(
+                        bottom = 44.dp,
+                        top = paddingValues.calculateTopPadding()
+                    )
                     .fillMaxSize()
                     .background(Color.White)
             ) {
                 PetitionList(petitions = viewModel.getPetition().collectAsLazyPagingItems(),
-                onClick = {petition ->
-                    val detailIntent: Intent = Intent(context, PetitionDetailActivity::class.java)
-                    detailIntent.putExtra(PetitionDetailActivity.PETITON_ID, petition.id.toLong())
-                    context.startActivity(detailIntent)
-                })
+                    onClick = { petition ->
+                        val detailIntent =
+                            Intent(context, PetitionDetailActivity::class.java)
+                        detailIntent.putExtra(
+                            PetitionDetailActivity.PETITON_ID,
+                            petition.id
+                        )
+                        context.startActivity(detailIntent)
+                    },
+                    isBottomPaddingEnabled = false)
             }
         }
     }
